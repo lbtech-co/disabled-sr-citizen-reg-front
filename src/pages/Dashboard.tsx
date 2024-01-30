a41309e4db920ac6a32e3eb5caff08fba437ec67
bibek <bamatya1@gmail.com>
2024-01-30 19:26:40 +0545

    refactor

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -2,6 +2,7 @@ import { Typography, Grid, Card } from "@mui/material";
 import Icon from "../components/Icon";
 import { DASHBOARD_CARD_ITEMS } from "../constants/constants";
 import { useTranslation } from "react-i18next";
+import { CSSProperties } from "react";
 
 function Dashboard() {
   const { t } = useTranslation();
@@ -10,24 +11,16 @@ function Dashboard() {
     <Grid container spacing={2}>
       {DASHBOARD_CARD_ITEMS.map((data) => (
         <Grid item xs={12} sm={6} md={4} lg={3} key={data.id}>
-          <Card
-            style={{
-              height: "90px",
-              display: "flex",
-              justifyContent: "center",
-              alignItems: "center",
-              gap: 10,
-              borderRadius: "8px",
-              background: "#0288D1",
-              boxShadow: "none",
-              padding: "2px 15px",
-            }}
-          >
+          <Card style={cardStyle}>
             <div style={{ width: "75%", color: "white" }}>
-              <Typography fontSize={"1.5rem"} lineHeight={1} fontWeight={600}>
+              <Typography
+                fontSize={"1.5rem"}
+                lineHeight={1}
+                sx={{ fontWeight: "bold" }}
+              >
                 {data.totalCount}
               </Typography>
-              <Typography fontSize={".9rem"}>{t(data.title)}</Typography>
+              <Typography variant="body2">{t(data.title)}</Typography>
             </div>
             <Icon
               sx={{ fontSize: "40px", color: "white", opacity: "0.4" }}
@@ -41,3 +34,15 @@ function Dashboard() {
 }
 
 export default Dashboard;
+
+const cardStyle: CSSProperties = {
+  height: "90px",
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+  gap: 10,
+  borderRadius: "8px",
+  background: "#0288D1",
+  boxShadow: "none",
+  padding: "2px 15px",
+};
