e269b6103a35a0c1013bf41d16645417aaaead5e
bibek <bamatya1@gmail.com>
2024-01-30 17:54:57 +0545

    add card background color

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,16 +1,30 @@
 import { Typography, Grid, Card } from "@mui/material";
 import Icon from "../components/Icon";
 
-function Dashboard() {
-  const CARD_ITEMS = [
-    { title: "Total numbers of disability", icon: "Home", id: "1" },
-    { title: "Total numbers of disability", icon: "Home", id: "2" },
-    { title: "Total numbers of disability", icon: "Home", id: "3" },
-    { title: "Total numbers of disability", icon: "Home", id: "4" },
-  ];
+const CARD_ITEMS = [
+  { title: "Total Disabilities", totalCount: 1075, icon: "Home", id: "1" },
+  { title: "Male Disabilities", totalCount: 57, icon: "Home", id: "2" },
+  { title: "Female Disabilities", totalCount: 32, icon: "Home", id: "3" },
+  { title: "Third Gender Disabilities", totalCount: 5, icon: "Home", id: "4" },
+  {
+    title: "Senior Citizen Disabilities",
+    totalCount: 17,
+    icon: "Home",
+    id: "5",
+  },
+  { title: "Senior Male Disabilities", totalCount: 6, icon: "Home", id: "6" },
+  { title: "Senior Female Disabilities", totalCount: 5, icon: "Home", id: "7" },
+  {
+    title: "Senior Third Gender Disabilities",
+    totalCount: 6,
+    icon: "Home",
+    id: "8",
+  },
+];
 
+function Dashboard() {
   return (
-    <Grid container spacing={3}>
+    <Grid container spacing={2}>
       {CARD_ITEMS.map((data) => (
         <Grid item xs={12} sm={6} md={4} lg={3} key={data.id}>
           <Card
@@ -20,13 +34,22 @@ function Dashboard() {
               justifyContent: "center",
               alignItems: "center",
               gap: 10,
+              borderRadius: "8px",
+              background: "#0288D1",
+              boxShadow: "none",
+              padding: "2px 15px",
             }}
           >
+            <div style={{ width: "75%", color: "white" }}>
+              <Typography fontSize={"1.5rem"} lineHeight={1} fontWeight={600}>
+                {data.totalCount}
+              </Typography>
+              <Typography fontSize={".9rem"}>{data.title}</Typography>
+            </div>
             <Icon
-              sx={{ fontSize: "40px", color: "grey.50" }}
+              sx={{ fontSize: "40px", color: "white", opacity: "0.4" }}
               name={data.icon}
             />
-            <Typography sx={{ width: "75%" }}>{data.title}</Typography>
           </Card>
         </Grid>
       ))}
